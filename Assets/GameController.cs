c089ec518084b1f50fe0a5d2ce423a5f9b166065
Isaac O'Donnell <odonnellisaac2020@gmail.com>
2023-03-15 11:56:01 -0300

    git commit;

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -7,12 +7,16 @@ public class GameController : MonoBehaviour
 
 public GameObject day;
 public GameObject night;
+public GameObject lightB;
+public GameObject darkB;
 public int dayOrNight;
 
     // Start is called before the first frame update
     void Start()
     {
         int dayOrNight = 0;
+        night.SetActive(false);
+        darkB.GetComponent<SpriteRenderer>().enabled = false;
     }
 
     // Update is called once per frame
@@ -23,11 +27,15 @@ public int dayOrNight;
             if (dayOrNight == 0){
                 day.SetActive(false);
                 night.SetActive(true);
+                lightB.GetComponent<SpriteRenderer>().enabled = false;
+                darkB.GetComponent<SpriteRenderer>().enabled = true;
                 dayOrNight = 1;
             }
             else{
                 day.SetActive(true);
                 night.SetActive(false);
+                lightB.GetComponent<SpriteRenderer>().enabled = true;
+                darkB.GetComponent<SpriteRenderer>().enabled = false;
                 dayOrNight = 0;
             }
         }
--- Assets/LBController.cs
+++ Assets/LBController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class LBController : MonoBehaviour
 {
+    public Animator anim;
+    public bool isMoving;
+    public GameObject darkMode;
+    public Animator darkAnim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       anim = GetComponent<Animator>(); 
+       isMoving = false;
+       darkAnim = darkMode.GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("right")){
+            isMoving = true;
+        }
+        if (Input.GetKeyUp("right")){
+            isMoving = false;
+        }
+
+        if (isMoving == true){
+            anim.Play("LBRunRight");
+            darkAnim.Play("DBRunRight");
+        }
+        else{
+            anim.Play("LBIdle");
+            darkAnim.Play("DBIdle");
+        }
     }
 }
