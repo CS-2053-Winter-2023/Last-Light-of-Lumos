ac1cf16fd209dd1918ccaab11cf5ef55027ab0f5
Isaac O'Donnell <odonnellisaac2020@gmail.com>
2023-03-27 16:43:58 -0300

    Added Menu Scene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public AudioSource menuMusic;
+    public GameObject title, start, end, next, background;
+    public int gameState;
+    public float duration;
+    float t;
+    Color color1 = new Color(1f,1f,1f,0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuMusic = GetComponent<AudioSource>();
+        menuMusic.Play();
+        gameState = 0;
+        t = 0;
+        start.SetActive(true);
+        end.SetActive(true);
+        next.SetActive(false);
+
+    }
+
+    public void LoadGame(){
+        gameState = 1;
+        t = 0;
+        start.SetActive(false);
+        end.SetActive(false);
+        next.SetActive(true);
+        if (title.GetComponent<SpriteRenderer>().color == new Color(1f,1f,1f,0f)){
+            SceneManager.LoadScene("LLOLStage1");
+        }
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    void Update(){
+        if (gameState == 0){
+            title.GetComponent<SpriteRenderer>().color = Color.Lerp(color1, Color.white, t);
+            t += Time.deltaTime / duration;
+        }
+        else {
+            title.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, color1, t);
+            background.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, Color.black, t);
+            t += Time.deltaTime / duration;
+        }
+
+    }
+
+}
